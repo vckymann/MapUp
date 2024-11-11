07014b4c2ed53ac6d8a8ec7d7fee8e589259cbd9
Vikas Singh Mann <vickymann9774@gmail.com>
2024-11-11 11:47:04 -0600

    put showpassword btn in input field.

--- src/components/Forms/Login.jsx
+++ src/components/Forms/Login.jsx
@@ -12,12 +12,15 @@ function Login ( ) {
             <div className="p-6 pt-0 grid gap-4">
                 <div className="grid gap-2">
                     <label className="text-sm font-medium leading-none">Email</label>
-                    <input name={"email"} label={"email"} type={"email"} {...register("email",{required: true})} className="flex h-10 w-full rounded-md border-green-500 border-input px-3 py-2 text-sm text-black" id="email" placeholder="m@example.com" autoComplete="on"/>
+                    <input name={"email"} label={"email"} type={"email"} {...register("email",{required: true})} className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm bg-black" id="email" placeholder="m@example.com" autoComplete="on"/>
                 </div>
-                <div className="grid gap-2">
+                <div className="grid gap-2 relative">
                         <label className="text-sm font-medium leading-none">Password</label>
-                        <input name={"password"} label={"password"} type={showPassword ? "text" : "password"} {...register("password",{required: true})} className="flex h-10 w-full rounded-md border-green-500 border-input px-3 py-2 text-sm text-black" id="password" placeholder="Enter your password"/>
-                        <button type="button" onClick={() => setShowPassword(!showPassword)} className="text-sm underline underline-offset-2">Show Password</button>
+                        <input name={"password"} label={"password"} type={showPassword ? "text" : "password"} {...register("password",{required: true})} className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm bg-black" id="password" placeholder="Enter your password"/>
+                        <button type="button" onClick={() => setShowPassword(!showPassword)} className="text-sm underline underline-offset-2 absolute right-1 top-7">{showPassword ? 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye p-1 hover:bg-[#80808067] rounded-md"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"/><circle className="fill-green-500 stroke-white" cx="12" cy="12" r="3"/></svg> : 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`lucide hover:bg-[#80808067] rounded-md p-1 lucide-eye`}><path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"/><path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"/><path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"/><line x1="2" x2="22" y1="2" y2="22"/></svg> 
+                    }</button>
                 </div>
             </div>        
             <div className="flex flex-col items-center gap-y-6 p-6 pt-0">
--- src/components/Forms/SignUp.jsx
+++ src/components/Forms/SignUp.jsx
@@ -14,14 +14,17 @@ function SignUpUser () {
             <div className="p-6 px-0 pt-0 grid gap-4">
                 <div className="grid gap-2">
                     <label className="text-sm text-white font-medium leading-none" htmlFor="name">Name</label>
-                        <input name={"name"} placeholder={"Enter your Name"} label={"Name"} type={"text"} {...register("name",{required: true})} className="flex h-10 w-full rounded-md border-green-500 bg-background px-3 py-2 text-sm text-black" id="name" />
+                        <input name={"name"} placeholder={"Enter your Name"} label={"Name"} type={"text"} {...register("name",{required: true})} className="flex h-10 w-full rounded-md border-2 bg-black px-3 py-2 text-sm text-white" id="name" />
                     <label className="text-white text-sm font-medium leading-none" htmlFor="email">Email</label>
-                        <input name={"email"} label={"Email"} type={"email"} {...register("email",{required: true})} className=" flex h-10 w-full rounded-md border-green-500 px-3 py-2 text-sm text-black" id="email" placeholder="m@example.com"/>
+                        <input name={"email"} label={"Email"} type={"email"} {...register("email",{required: true})} className=" flex h-10 w-full rounded-md border-2 bg-black px-3 py-2 text-sm text-white" id="email" placeholder="m@example.com"/>
                 </div>
-                <div className="grid gap-2">
+                <div className="grid gap-2 relative">
                     <label className="text-sm text-white font-medium leading-none" htmlFor="password">Password</label>
-                        <input name={"password"} label={"Password"} type={showPassword ? "text" : "password"} {...register("password",{required: true})} className="flex h-10 w-full rounded-md border-green-500 px-3 py-2 text-sm text-black" id="password" placeholder="Enter your password"/>
-                    <button type="button" onClick={() => setShowPassword(!showPassword)} className="text-sm underline underline-offset-2">Show Password</button>
+                        <input name={"password"} label={"Password"} type={showPassword ? "text" : "password"} {...register("password",{required: true})} className="flex h-10 w-full rounded-md border-2 bg-black px-3 py-2 text-sm text-white" id="password" placeholder="Enter your password"/>
+                    <button type="button" onClick={() => setShowPassword(!showPassword)} className="text-sm underline underline-offset-2 absolute right-1 top-7">{showPassword ? 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye p-1 hover:bg-[#80808067] rounded-md"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"/><circle className="fill-green-500 stroke-white" cx="12" cy="12" r="3"/></svg> : 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`lucide hover:bg-[#80808067] rounded-md p-1 lucide-eye`}><path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"/><path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"/><path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"/><line x1="2" x2="22" y1="2" y2="22"/></svg> 
+                    }</button>
                 </div>
             </div>
             <div className="flex flex-col items-center gap-y-6 p-6 pt-0">
